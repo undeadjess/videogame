3705171e29c00f32dca96508c6f67791ee494b71
Jess <68990356+undeadjess@users.noreply.github.com>
2024-05-30 14:19:40 +1200

    add: Basic menu elements

---
+++ src/scripts/menu/buttons.gd
@@ -0,0 +1,15 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_anything_pressed():
+		print("click")
+		# change scene
+		get_tree().change_scene_to_file("res://scenes/testScene.tscn")
+	pass
