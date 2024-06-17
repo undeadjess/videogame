8b9066c7787eb89914f9accbc2aa7ae99a11c664
Jess <68990356+undeadjess@users.noreply.github.com>
2024-06-18 11:58:58 +1200

    Add script to make menu functional (options and credits still dont work)

---
+++ src/scripts/menus/mainMenu.gd
@@ -0,0 +1,28 @@
+extends MarginContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+# play button
+func _on_buttonPlay_pressed():
+	get_tree().change_scene_to_file("res://scenes/testScene.tscn")
+
+
+func _on_buttonOptions_pressed():
+	print("options button pressed")
+
+
+func _on_buttonCredits_pressed():
+	print("credits button pressed")
+
+
+func _on_buttonExit_pressed():
+	print("exit button pressed, closing")
+	get_tree().quit()
