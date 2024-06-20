9428feb8e331234c349e97bb1527d733a586d8ef
AliahX <40045439+AliahX@users.noreply.github.com>
2024-06-20 12:59:03 +1200

    Adjust monster speed, rename variables, & remove unused code

--- src/assets/objects/characters/monster.gd
+++ src/assets/objects/characters/monster.gd
@@ -3,23 +3,22 @@ extends CharacterBody3D
 
 signal playerCollide
 
-var SPEED = 1.0
+var speedMultiplier = 3.0
 var pathing = true
 var startPathing = false
 var time = 0
 
 @onready var nav_agent = $NavigationAgent3D
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var head := $Head
 @onready var camera := $Head/Camera3D
 
 func _physics_process(_delta):
 	if(startPathing && pathing):
 		time += 0.05
-		SPEED = (sin(time) / 2 + 0.5)
+		var speed = (sin(time) / 2 + 0.5) * speedMultiplier
 		var current_location = global_transform.origin
 		var next_location = nav_agent.get_next_path_position()
-		var new_velocity = (next_location - current_location).normalized() * SPEED
+		var new_velocity = (next_location - current_location).normalized() * speed
 		
 		velocity = velocity.move_toward(new_velocity, 0.25)
 		move_and_slide()
