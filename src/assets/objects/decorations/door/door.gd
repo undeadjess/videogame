e5c3a6d650f7a68511d458e319cef876111b2a16
AliahX <40045439+AliahX@users.noreply.github.com>
2024-07-01 20:24:48 +1200

    Stop player from closing the door on themselves or the monster

--- src/assets/objects/decorations/door/door.gd
+++ src/assets/objects/decorations/door/door.gd
@@ -14,5 +14,13 @@ func _physics_process(_delta):
 		interact = false
 		open = !open
 	
-	part1.rotation.y = baseRot + (90 if open else 0)
-	part2.rotation.y = baseRot - (90 if open else 0)
+	var playerPos = get_parent_node_3d().player.position
+	if playerPos.distance_to(global_position) < 1.125 && !open:
+		open = true
+	
+	var monsterPos = get_parent_node_3d().monster.position
+	if monsterPos.distance_to(global_position) < 1.4 && !open:
+		open = true
+	
+	part1.rotation.y = baseRot + (1.5 if open else 0)
+	part2.rotation.y = baseRot - (1.5 if open else 0)
