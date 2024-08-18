e08102d8d05e81954719c6438c862814bdaa8c03
AliahX <40045439+AliahX@users.noreply.github.com>
2024-08-19 11:17:00 +1200

    Made locked an exported property rather than metadata, and made it show up in the editor.

--- src/assets/objects/decorations/door/door.gd
+++ src/assets/objects/decorations/door/door.gd
@@ -1,44 +1,44 @@
+@tool
 extends StaticBody3D
 
 const obj = "door"
 var open = false
 var interact = false
-var locked = false
+
+@export var locked: bool
 
 @onready var part1 = $Part1
 @onready var part2 = $Part2
 @onready var baseRot = part1.rotation.y
 
-func _ready():
-	locked = get_meta("locked", false)
-
 func _physics_process(_delta):
-	var player = get_parent_node_3d().player
-	var monster = get_parent_node_3d().monster
-	var playerPos = player.position
-	var monsterPos = monster.position
-	
 	$Lock.visible = locked
-	
-	if interact:
-		interact = false
-		if !locked:
-			if open:
-				if !monsterPos.distance_to(global_position) < 1.4:
-					open = false
+	if !Engine.is_editor_hint():
+		var player = get_parent_node_3d().player
+		var monster = get_parent_node_3d().monster
+		var playerPos = player.position
+		var monsterPos = monster.position
+		
+		
+		if interact:
+			interact = false
+			if !locked:
+				if open:
+					if !monsterPos.distance_to(global_position) < 1.4:
+						open = false
+				else:
+					open = true
 			else:
-				open = true
-		else:
-			if player.inventory.keys.keys > 0:
-				locked = false
-				
-	
-	if playerPos.distance_to(global_position) < 1.125 && !open && !locked:
-		open = true
-	
-	if monsterPos.distance_to(global_position) < 1.4 && !open && !locked:
-		monster.time = 3
-		open = true
-	
-	part1.rotation.y = baseRot + (deg_to_rad(90) if open else 0)
-	part2.rotation.y = baseRot - (deg_to_rad(90) if open else 0)
+				if player.inventory.keys.keys > 0:
+					locked = false
+					
+		
+		if playerPos.distance_to(global_position) < 1.125 && !open && !locked:
+			open = true
+		
+		if monsterPos.distance_to(global_position) < 1.4 && !open && !locked:
+			monster.time = 3
+			open = true
+		
+		part1.rotation.y = baseRot + (deg_to_rad(90) if open else 0)
+		part2.rotation.y = baseRot - (deg_to_rad(90) if open else 0)
